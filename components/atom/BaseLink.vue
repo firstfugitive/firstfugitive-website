5e544a5a6a5b7892aba00377b4745eaa5b4b8d2d
Ben Jacobsen <ben.jacobsen@unitb-solution.de>
2021-11-08 12:37:22 +0100

    improve link

--- components/atom/BaseLink.vue
+++ components/atom/BaseLink.vue
@@ -21,15 +21,36 @@ export default {
 </script>
 
 
-<style lang="scss" scoped>
-.base-link {
+<style lang="scss" >
+@import "assets/css/variables";
 
-  .link {
-    border: 2px solid black;
-    padding: 1rem 2rem;
-    &:hover {
-        background: rgba(220, 171, 171, 0.1);
-    }
+.base-link {
+  position: relative;
+  border: 2px solid black;
+	padding: 1rem 2rem;
+	&:hover {
+			background: rgba(220, 171, 171, 0.1);
+	}
+
+	&::before {
+		content: '';
+		position: absolute;
+		z-index: 0;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 0%;
+		transition: height 0.5s;
+		background-color: $color-soft-white;
+	}
+
+	&:hover::before {
+		height: 100%;
+	}
+
+	.link {
+		position: relative;
+		z-index: 100;
 
     text-decoration: none;
     &:visited {
