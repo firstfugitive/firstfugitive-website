9428d95c395a5e898af26afc0cd9ff5a048521e3
Ben Jacobsen <ben.jacobsen@unitb-solution.de>
2022-01-16 17:09:23 +0100

    improve blog overview

--- components/pageContent/ContentBlogOverview.vue
+++ components/pageContent/ContentBlogOverview.vue
@@ -4,17 +4,11 @@
       Read through some blogs
     </div>
     <div class="content-blog-overview__blogs">
-      <nuxt-link class="content-blog-overview__blog-teaser"
-        :to="getUrlFromBlog(item)"
-        v-for="(item, index) in items" 
-        :key="`blog-teaser_${index}`">
-        <img class="content-blog-overview__blog-image" :src="getImageSrcFromBlog(item)" />
-        <div class="content-blog-overview__blog-title">
-          {{ getTitleFromBlog(item) }}
-        </div>
-      </nuxt-link>
+      <blog-teaser
+        v-for="(item, index) in items"
+        :key="`blog-teaser_${index}`"
+        :blogPage="item"></blog-teaser>
     </div>
-
     <component
       v-for="item in content"
 	    :is="item.contentType"
@@ -27,10 +21,11 @@
 
 <script>
 import ImageFullSize from '../molecule/ImageFullSize.vue';
+import BlogTeaser from '../molecule/BlogTeaser.vue';
 import BlogService from '../../assets/js/BlogService.js';
 import { getUrlFromPage } from '../../assets/js/Util.js';
 export default {
-  components: { ImageFullSize },
+  components: { ImageFullSize, BlogTeaser },
   name: "ContentBlogOverview",
   props: {
     data: {},
@@ -88,40 +83,21 @@ export default {
   }
   &__blogs {
     display: flex;
-    flex-wrap: wrap;
+    flex-flow: row wrap;
     justify-content: center;
-    gap: 1rem;
-    @include mb-down($mobile-max) {
-      margin: 0 5%;
-    }
-  }
-  &__blog-teaser {
-    text-align: center;
-    max-width: 200px;
-    text-decoration: none;
-    @extend %boxShadow;
-    border-radius: 8px;
-    color: $dark-text;
-    &:visited {
-      color: $dark-text;
-      text-decoration: none;
-    }
-    &:hover {
-
-    }
-    img {
-      max-width: 100%;
-      border-top-right-radius: 8px;
-      border-top-left-radius: 8px;
-    }
-  }
-  &__blog-title {
-    padding: 0.6rem 1.3rem;
+    align-items: stretch;
+    margin: 0 5%;
+    gap: 1.5rem;
     @include mb-up($tablet-min) {
-      padding: 0.8rem 1.6rem;
+      margin: 0 1.5rem;
+      gap: 2rem;
     }
     @include mb-up($desktop-min) {
-      padding: 1rem 2rem;
+      margin: 0 4rem;
+      gap: 3rem;
+    }
+    @include mb-up($desktop-extended) {
+      margin: 0 15rem;
     }
   }
 }
